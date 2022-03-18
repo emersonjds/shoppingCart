45d102a6042b3c2c7c5994e98a8e4ca2a3f673ce
Emerson Silva <emerson_jdss@hotmail.com>
2022-03-18 18:25:12 -0300

    config BoxItem

---
+++ src/components/BoxItem/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const BoxItem: React.FC = () => {
+  return <View />;
+}
+
+export default BoxItem;
