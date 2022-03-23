5691a9d7996505b921c2a956fcdf0854cf7db19a
Emerson Silva <emerson_jdss@hotmail.com>
2022-03-23 19:11:54 -0300

    adjust error on details

--- src/screens/Details/styles.ts
+++ src/screens/Details/styles.ts
@@ -1,5 +1,10 @@
 import styled from 'styled-components/native';
 
+export const Container = styled.View`
+  flex: 1;
+  background: #fff;
+`;
+
 export const TopContainer = styled.View`
   height: 80%;
   width: 100%;
@@ -12,6 +17,26 @@ export const BottomContainer = styled.View`
   padding: 20px;
   align-items: center;
   justify-content: center;
+`;
+
+export const ContainerProductDetails = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+  margin-top: 25px;
+  padding-left: 30px;
+  padding-right: 30px;
+`;
+
+export const TitleProduct = styled.Text`
+  color: #333;
+  font-size: 18px;
+  text-align: center;
+`;
+
+export const TitlePrice = styled.Text`
+  color: #ea4c89;
+  font-size: 18px;
 `;
 
 export const ButtonPay = styled.TouchableOpacity`
