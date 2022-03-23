6bf47daafab5eb15fdfa25d6bf74b46facb079c3
Emerson Silva <emerson_jdss@hotmail.com>
2022-03-23 17:27:07 -0300

    add ratings and configure Resume

---
+++ src/screens/Resume/index.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import { View } from 'react-native';
+import { useSelector } from 'react-redux';
+import { shoppingSelector } from '../../redux/slices/shoppingSlice';
+
+// import { Container } from './styles';
+
+const Resume: React.FC = () => {
+  const { cart } = useSelector(shoppingSelector);
+
+  useEffect(() => {
+    console.log('DADOS', cart);
+  }, []);
+
+  return <View />;
+};
+
+export default Resume;
