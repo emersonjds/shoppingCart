f6e60c817d6ad4cf5e11a6e11ab0b3dcf360d8d2
Emerson Silva <emerson_jdss@hotmail.com>
2022-03-23 22:42:44 -0300

    adjust data on Resume

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,18 +1,49 @@
-import React, { useEffect } from 'react';
-import { View } from 'react-native';
+import React from 'react';
+import { Image, ScrollView, Text } from 'react-native';
 import { useSelector } from 'react-redux';
+import { Box, Container } from '../../components/Spacing';
 import { shoppingSelector } from '../../redux/slices/shoppingSlice';
-
-// import { Container } from './styles';
 
 const Resume: React.FC = () => {
   const { cart } = useSelector(shoppingSelector);
 
-  useEffect(() => {
-    console.log('DADOS', cart);
-  }, []);
-
-  return <View testID="Resume" />;
+  return (
+    <Container>
+      <ScrollView>
+        {cart.map(product => (
+          <Box
+            height={100}
+            width={'100%'}
+            p={10}
+            flexDirection="row"
+            alignItems={'center'}>
+            <Box width={'20%'}>
+              <Image
+                source={{ uri: product.image }}
+                style={{ width: 70, height: 70 }}
+              />
+            </Box>
+            <Box width={'50%'} alignItems={'center'}>
+              <Text
+                style={{
+                  color: '#000',
+                }}>
+                {product.title}
+              </Text>
+            </Box>
+            <Box width={'30%'} alignItems={'center'}>
+              <Text
+                style={{
+                  color: '#000',
+                }}>
+                {product.quantity}
+              </Text>
+            </Box>
+          </Box>
+        ))}
+      </ScrollView>
+    </Container>
+  );
 };
 
 export default Resume;
