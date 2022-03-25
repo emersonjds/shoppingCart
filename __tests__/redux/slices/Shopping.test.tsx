d3fa63aad36bf39aa4f960524e43e7e4741c12ba
Emerson Silva <emerson_jdss@hotmail.com>
2022-03-25 20:02:50 -0300

    adjust shopping slice tests

--- __tests__/redux/slices/Shopping.test.tsx
+++ __tests__/redux/slices/Shopping.test.tsx
@@ -3,8 +3,8 @@ import {
   addToCart,
   removeAllFromCart,
   removeQuantityOfItem,
-} from '../../../../src/redux/slices/shoppingSlice';
-import store from '../../../../src/redux/store';
+} from '../../../src/redux/slices/shoppingSlice';
+import store from '../../../src/redux/store';
 
 describe('shopping slice', () => {
   let item = {
@@ -15,24 +15,24 @@ describe('shopping slice', () => {
     price: 10,
   };
 
-  test('should add item on cart', () => {
+  it('should add item on cart', () => {
     store.dispatch(addToCart(item));
     expect(store.getState().shopping.cart).toEqual([item]);
   });
 
-  test('should add quantity of item on cart', () => {
+  it('should add quantity of item on cart', () => {
     let product = { ...item, quantity: 1 };
     store.dispatch(addQuantityOfItem(product));
     expect(store.getState().shopping.cart[0]?.quantity).toEqual(2);
   });
 
-  test('should remove quantity of item on cart', () => {
+  it('should remove quantity of item on cart', () => {
     let product = { ...item, quantity: 2 };
     store.dispatch(removeQuantityOfItem(product));
     expect(store.getState().shopping.cart[0]?.quantity).toEqual(1);
   });
 
-  test('should remove all items from cart', () => {
+  it('should remove all items from cart', () => {
     store.dispatch(removeAllFromCart());
     expect(store.getState().shopping.cart).toEqual([]);
   });
