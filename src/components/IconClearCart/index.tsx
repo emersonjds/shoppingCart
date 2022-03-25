86a09ec00f684144e6d2b997fe7fa291b595d0d8
Emerson Silva <emerson_jdss@hotmail.com>
2022-03-24 22:50:06 -0300

    config trash icon for remove all items

---
+++ src/components/IconClearCart/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const IconClearCart: React.FC = () => {
+  return <View />;
+};
+
+export default IconClearCart;
