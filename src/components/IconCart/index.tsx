1b0e76e0ff04996bdf72b38959b9a75e89039957
Emerson Silva <emerson_jdss@hotmail.com>
2022-03-24 22:37:53 -0300

    adjust slice in icon Cart

---
+++ src/components/IconCart/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+import { Box } from '../Spacing';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { useSelector } from 'react-redux';
+import { shoppingSelector } from '../../redux/slices/shoppingSlice';
+import { useNavigation } from '@react-navigation/native';
+import Colors from '../../contants';
+
+const IconCart: React.FC = () => {
+  const { cart } = useSelector(shoppingSelector);
+  const navigation = useNavigation();
+
+  const itemsOnCart = cart.reduce((acc, item) => acc + item.quantity, 0);
+
+  return (
+    <TouchableOpacity onPress={() => navigation.navigate('Resume')}>
+      <Box
+        height={20}
+        width={20}
+        bg={Colors.PRIMARY_COLOR}
+        style={{
+          position: 'absolute',
+          zIndex: 1,
+          right: 10,
+          borderRadius: 10,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Text
+          style={{
+            color: '#fff',
+          }}>
+          {itemsOnCart}
+        </Text>
+      </Box>
+      <Icon name="shopping-cart" size={30} color="#000" />
+    </TouchableOpacity>
+  );
+};
+
+export default IconCart;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Details from '../screens/Details';
 import Home from '../screens/Home';
 import Resume from '../screens/Resume';
-import Icon from 'react-native-vector-icons/FontAwesome';
 import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import IconCart from '../components/IconCart';
 
 const Stack = createNativeStackNavigator();
 
@@ -19,13 +19,23 @@ const Routes = () => {
           options={{
             headerTitleAlign: 'center',
             headerLeft: () => <MCIcon name="menu" size={30} color="#000" />,
-            headerRight: () => (
-              <Icon name="shopping-cart" size={30} color="#000" />
-            ),
+            headerRight: () => <IconCart />,
           }}
         />
-        <Stack.Screen name="Details" component={Details} />
-        <Stack.Screen name="Resume" component={Resume} />
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          options={{
+            headerTitleAlign: 'center',
+          }}
+        />
+        <Stack.Screen
+          name="Resume"
+          component={Resume}
+          options={{
+            headerTitleAlign: 'center',
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
