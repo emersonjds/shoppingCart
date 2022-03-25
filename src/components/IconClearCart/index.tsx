7d183891c992b8ede904d3e7604d02cf81cf4a2a
Emerson Silva <emerson_jdss@hotmail.com>
2022-03-25 15:44:25 -0300

    add message for remove data from cart

--- src/components/IconClearCart/index.tsx
+++ src/components/IconClearCart/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { Alert, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { useDispatch } from 'react-redux';
 import { removeAllFromCart } from '../../redux/slices/shoppingSlice';
@@ -8,8 +8,26 @@ import { Box } from '../Spacing';
 const IconClearCart: React.FC = () => {
   const dispatch = useDispatch();
 
+  const messageFromRemoveData = () => {
+    Alert.alert(
+      'Remover todos os itens do carrinho?',
+      '',
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+        },
+        {
+          text: 'Remover',
+          onPress: () => dispatch(removeAllFromCart()),
+        },
+      ],
+      { cancelable: false },
+    );
+  };
+
   return (
-    <TouchableOpacity onPress={() => dispatch(removeAllFromCart())}>
+    <TouchableOpacity onPress={() => messageFromRemoveData()}>
       <Box>
         <Icon name="trash" size={30} color="#000" />
       </Box>
